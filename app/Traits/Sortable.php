7d41590505056af242a3cde12899c02ba1674e07
xyz <xyz@farmcrowdy.com>
2022-06-04 13:57:09 +0100

    refactored sortColumn scope to reusuable trait

---
+++ app/Traits/Sortable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+use InvalidArgumentException;
+
+trait Sortable 
+{
+    /**
+     * sorts a single column of a model table
+     *
+     * @param Builder $query
+     * @param string $column
+     * @param string $order
+     * @return Builder
+     */
+    public function scopeSortColumn(Builder $query, $column, $order ="desc") :Builder
+    {
+        return $query->orderBy($column, $order);
+    }
+
+    /**
+     * sorts multiple columns of a model table
+     *
+     * @param Builder $query
+     * @param array $sorts
+     * @return Builder
+     */
+    public function scopeSortColumns(Builder $query, array $sorts) :Builder
+    {
+        $orders = ["asc", "desc"];
+
+        foreach ($sorts as $column => $by) {
+
+            if (!in_array($by, $orders)) {
+                throw new InvalidArgumentException("Invalid sort order $by");
+            }
+            
+            $query->orderBy($column, $by);
+        }
+
+        return $query;
+    }
+}
