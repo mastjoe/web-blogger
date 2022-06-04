2cd19cea24d0f47208865b52f3c483d5ccd68407
mastjoe <xyz@farmcrowdy.com>
2022-06-04 15:54:53 +0100

    updated .env.example with necessary variables

--- config/blogger.php
+++ config/blogger.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'feed_link' => 'https://sq1-api-test.herokuapp.com/posts',
+    'feed_link' => env('FEED_LINK', 'https://sq1-api-test.herokuapp.com/posts'),
     'admin'     => [
         'name'     => env('APP_ADMIN_NAME', 'admin'),
         'email'    => env('APP_ADMIN_EMAIL', 'admin@email.com'),
